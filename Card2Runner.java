248b86f59d5496bed1047ed3f93a9610b08e52f5
leopup <leonardo.montanez-s@techhigh.us>
2016-01-27 07:59:31 -0500

    Deck

---
+++ Card2Runner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class Card2Runner{
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		public ArrayList<Cards> deckList = new ArrayList<Cards>();
+	}
+}
