61e06644d3f3d8b233733c9a459409f132457807
leopup <leonardo.montanez-s@techhigh.us>
2016-01-27 08:01:07 -0500

    y

--- Card2Runner.java
+++ Card2Runner.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * This is a class that tests the Deck class.
  */
@@ -9,6 +9,37 @@ public class Card2Runner{
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		public ArrayList<Cards> deckList = new ArrayList<Cards>();
+		Scanner scnr = new Scanner(System.in);
+		String[] suitList;
+		String[] rankList;
+		int[] pointList;
+		
+		System.out.println("Welcome to the deck builder.");
+		System.out.println("Please input the amount of suits your cards will have.(Like diamond or spades)");
+		int suitSize = scnr.nextInt();
+		suitList = new String[suitSize];
+		System.out.println("Please input the amount of ranks your cards will have.(Like King or Ace)");
+		int rankSize = scnr.nextInt();
+		rankList = new String[rankSize];
+		System.out.println("Please input the amount of values of your cards in your desired order.");
+		int pointSize = scnr.nextInt();
+		pointList = new int[pointSize];
+		
+		System.out.println("Input the name of your card suits please.(In order pls :^) )");
+		for(int i = 0; i < suitSize-1; i++){
+			if(i > 0){
+				suitList[i] = scnr.next();
+			}
+			else{
+				System.out.println("Another one...");
+				suitList[i] = scnr.next();
+			}
+		}
+		System.out.println("Input the name of your card ranks please.(In order pls :^) )");
+		for(int j = 0; j < rankSize-1; j++){
+			if(j > 0){
+				
+			}
+		}
 	}
 }
